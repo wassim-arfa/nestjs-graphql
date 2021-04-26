478c0ffb1f619280603194ae2049a1a79bb60883
Wassim Arfa <wassim.arfa@gmail.com>
2021-04-26 14:54:05 +0100

    Setup TypeORM
    npm install typeorm @nestjs/typeorm mongodb @types/mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: []
+    }),
+
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
